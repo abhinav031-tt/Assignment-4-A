86f30cae6e0cb8d4301a62be932d3e8d6faba64f
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-02 12:19:45 +0530

    Add files via upload

---
+++ lambda_argument.py
@@ -0,0 +1,12 @@
+'''
+
+Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+sample input: 10
+
+
+'''
+
+
+x = lambda a : a + 10
+print(x(25))
